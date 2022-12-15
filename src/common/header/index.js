7dad1ba225d5697db91f3e27bb6ee1d54578ab6d
MiqiPei <peiminqi@gmail.com>
2022-12-14 20:32:56 -0800

    get popular topics using Ajax

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
 import  { actionCreators } from './store';
@@ -18,70 +18,74 @@ import {
     SearchInfoList
 } from './style.js';
 
-const getListArea = (show) => {
-  if (show) {
+class Header extends Component{
+  getListArea() {
+    if (this.props.focused) {
+      return(
+        <SearchInfo>
+          <SearchInfoTitle>HOT!!!
+            <SearchInfoSwitch>
+              Shuffle
+            </SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>
+            {
+              this.props.list.map((item) => {
+                return <SearchInfoItem key={item}>{item}</SearchInfoItem>
+              })
+            }
+          </SearchInfoList>
+        </SearchInfo>
+      )
+    }else {
+      return null;
+    }
+  }
+
+  render(){
     return(
-      <SearchInfo>
-        <SearchInfoTitle>HOT!!!
-          <SearchInfoSwitch>
-            Shuffle
-          </SearchInfoSwitch>
-        </SearchInfoTitle>
-        <SearchInfoList>
-          <SearchInfoItem>jokes</SearchInfoItem>
-          <SearchInfoItem>jokes</SearchInfoItem>
-          <SearchInfoItem>jokes</SearchInfoItem>
-          <SearchInfoItem>jokes</SearchInfoItem>
-          <SearchInfoItem>jokes</SearchInfoItem>
-          <SearchInfoItem>jokes</SearchInfoItem>
-          <SearchInfoItem>jokes</SearchInfoItem>
-        </SearchInfoList>
-      </SearchInfo>
-    )
-  }else {
-    return null;
+      <HeaderWrapper>
+            <><Logo href='/' />
+        <Nav>
+        <NavItem className='left active'>Homepage</NavItem>
+        <NavItem className='left'>Downloa App</NavItem>
+        <NavItem className='right'>Login</NavItem>
+        <NavItem className='right'>
+          <i className="iconfont">&#xe636;</i>
+        </NavItem>
+        <SearchWrapper>
+          <CSSTransition in={this.props.focused} timeout={200} classNames="slide">
+            <NavSearch
+              className={this.props.focused ? 'focused' : ''}
+              onFocus={this.props.handleInputFocus}
+              onBlur={this.props.handleInputBlur}></NavSearch>
+          </CSSTransition>
+          <i className={this.props.focused ? 'focused iconfont' : 'iconfont'}>&#xe614;</i>
+          {this.getListArea()}
+        </SearchWrapper>
+      </Nav><Addition>
+          <Button className='writting'><i className="iconfont">&#xe615;</i>Post</Button>
+          <Button className='reg'>Register</Button>
+        </Addition></>
+      </HeaderWrapper>
+    );   
+
   }
 }
 
-const Header = (props) => {
-  return(
-    <HeaderWrapper>
-          <><Logo href='/' />
-      <Nav>
-      <NavItem className='left active'>Homepage</NavItem>
-      <NavItem className='left'>Downloa App</NavItem>
-      <NavItem className='right'>Login</NavItem>
-      <NavItem className='right'>
-        <i className="iconfont">&#xe636;</i>
-      </NavItem>
-      <SearchWrapper>
-        <CSSTransition in={props.focused} timeout={200} classNames="slide">
-          <NavSearch
-            className={props.focused ? 'focused' : ''}
-            onFocus={props.handleInputFocus}
-            onBlur={props.handleInputBlur}></NavSearch>
-        </CSSTransition>
-        <i className={props.focused ? 'focused iconfont' : 'iconfont'}>&#xe614;</i>
-        {getListArea(props.focused)}
-      </SearchWrapper>
-    </Nav><Addition>
-        <Button className='writting'><i className="iconfont">&#xe615;</i>Post</Button>
-        <Button className='reg'>Register</Button>
-      </Addition></>
-    </HeaderWrapper>
-  )    
-}
 
 
 const mapStateToProps = (state) => {
   return{
-    focused: state.getIn(['header','focused'])
+    focused: state.getIn(['header','focused']),
+    list: state.getIn(['header', 'list'])
   }
 }
 
 const mapDispathToProps = (dispatch) =>{
   return{
     handleInputFocus() {
+      dispatch(actionCreators.getList());
       dispatch(actionCreators.searchFocus());
     },
     handleInputBlur(){
