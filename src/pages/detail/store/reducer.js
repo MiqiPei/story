8098923d913fcf55a530c4361ac7301b8afe3fba
MiqiPei <peiminqi@gmail.com>
2022-12-20 15:34:38 -0800

    setup reducer for detail page

---
+++ src/pages/detail/store/reducer.js
@@ -0,0 +1,16 @@
+import { fromJS } from 'immutable';
+import * as constants from './constants';
+
+const defaultState = fromJS({
+    title: 'What is "teamwork" to you?',
+    content: '<img src="http://www.designerspics.com/wp-content/uploads/2014/10/paper_people_free_photo1.jpg"/><p><b>Teamwork is generally understood as the willingness of a group of people to work together to achieve a common aim. For example we often use the phrase:” he or she is a good team player”. This means someone has the interests of the team at heart, working for the good of the team.</b></p><p>Teamwork is generally understood as the willingness of a group of people to work together to achieve a common aim. For example we often use the phrase:” he or she is a good team player”. This means someone has the interests of the team at heart, working for the good of the team.</p><p>Teamwork is generally understood as the willingness of a group of people to work together to achieve a common aim. For example we often use the phrase:” he or she is a good team player”. This means someone has the interests of the team at heart, working for the good of the team.</p><p>Teamwork is generally understood as the willingness of a group of people to work together to achieve a common aim. For example we often use the phrase:” he or she is a good team player”. This means someone has the interests of the team at heart, working for the good of the team.</p>'
+});
+export default (state = defaultState, action) => {
+    switch(action.type) {
+        
+                
+        default:
+            return state;
+    }
+    
+}
